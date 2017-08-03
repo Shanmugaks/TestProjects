76ed84d8214a410015bf1d753453eeeba781a7b4
Shanmuga <notifyshan@gmail.com>
2017-08-03 16:33:57 +1000

    Db Synchronizer fixes

---
+++ ThreadSafeDBService/Controllers/OS_2fAuthorisation_UpdValidateAuthCodeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Threading;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ThreadSafeDBService.Controllers
+{
+    /// <summary>
+    /// Description for ThreadSafeOutProcessController.
+    /// </summary>
+    public class OS_2fAuthorisation_UpdValidateAuthCodeController : ApiController
+    {
+        protected string ConnectionString;
+        protected string MutexName;
+        protected string ProcName;
+
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IHttpActionResult Post(int iSystemID, int iCallingContext, string sAccountID, string sMobileNo, string sAuthorisationCode, int iExpiryMinutes)
+        {
+            IHttpActionResult Result = Ok("Success");
+            Console.WriteLine("SyncAllController - START({0})", Thread.CurrentThread.ManagedThreadId);
+            try
+            {
+                Console.WriteLine("SyncAllController - TRY START({0})", Thread.CurrentThread.ManagedThreadId);
+
+                MutexName = "Global\\" + "ThreadSafeDBService" + "ALL";
+                ProcName = "OS_2fAuthorisation_UpdValidateAuthCode";
+                ConnectionString = "data source=SHANMUGAPC\\SQLEXPRESS;initial catalog=ShanDB;Integrated Security=true;";
+
+                // create a global mutex
+                using (var mutex = new Mutex(false, MutexName))
+                {
+                    Console.WriteLine("Waiting for mutex");
+                    var mutexAcquired = false;
+                    try
+                    {
+                        // acquire the mutex (or timeout after 60 seconds)
+                        // will return false if it timed out
+                        mutexAcquired = mutex.WaitOne(60000);
+                    }
+                    catch (AbandonedMutexException)
+                    {
+                        // abandoned mutexes are still acquired, we just need
+                        // to handle the exception and treat it as acquisition
+                        mutexAcquired = true;
+                    }
+
+                    // if it wasn't acquired, it timed out, so can handle that how ever we want
+                    if (!mutexAcquired)
+                    {
+                        Result = InternalServerError(new Exception("Not able to acquire Mutex"));
+                    }
+                    // otherwise, we've acquired the mutex and should do what we need to do,
+                    // then ensure that we always release the mutex
+                    try
+                    {
+                        using (SqlConnection ConnectionObject = new SqlConnection(ConnectionString))
+                        {
+                            using (SqlCommand Sqlcommand = new SqlCommand(ProcName, ConnectionObject))
+                            {
+                                Sqlcommand.CommandType = CommandType.StoredProcedure;
+                                Sqlcommand.Parameters.Add("@iSystemID", SqlDbType.Int).Value = iSystemID;
+                                Sqlcommand.Parameters.Add("@iCallingContext", SqlDbType.Int).Value = iCallingContext;
+                                Sqlcommand.Parameters.Add("@sAccountID", SqlDbType.VarChar, 255).Value = sAccountID;
+                                Sqlcommand.Parameters.Add("@sMobileNo", SqlDbType.VarChar, 20).Value = sMobileNo;
+                                Sqlcommand.Parameters.Add("@sAuthorisationCode", SqlDbType.VarChar, 128).Value = sAuthorisationCode;
+                                Sqlcommand.Parameters.Add("@iExpiryMinutes", SqlDbType.Int).Value = iExpiryMinutes;
+                                ConnectionObject.Open();
+                                Sqlcommand.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        mutex.ReleaseMutex();
+                    }
+                }
+                Console.WriteLine("SyncAllController - TRY END({0})", Thread.CurrentThread.ManagedThreadId);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("SyncAllController - CATCH START({0})", Thread.CurrentThread.ManagedThreadId);
+                Result = InternalServerError(e);
+                Console.WriteLine("SyncAllController - CATCH END({0})", Thread.CurrentThread.ManagedThreadId);
+            }
+            Console.WriteLine("SyncAllController - END({0})", Thread.CurrentThread.ManagedThreadId);
+            return Result;
+        }
+    }
+}
--- ThreadSafeDBService/Startup.cs
+++ ThreadSafeDBService/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Owin;
+using System;
 using System.Net.Http.Headers;
 using System.Web.Http;
 using System.Web.Http.Cors;
@@ -16,6 +17,32 @@ namespace ThreadSafeDBService
         public void Configuration(IAppBuilder appBuilder)
         {
             HttpConfiguration config = new HttpConfiguration();
+
+            /*
+            //static settings
+            appBuilder.Use()
+                .MaxBandwidth(10000) //bps
+                .MaxConcurrentRequests(10)
+                .ConnectionTimeout(TimeSpan.FromSeconds(10))
+                .MaxQueryStringLength(15) //Unescaped QueryString
+                .MaxRequestContentLength(15)
+                .MaxUrlLength(20);
+           */    
+
+            /*
+            //dynamic settings
+            appBuilder.Use()
+                .MaxBandwidth(() => 10000) //bps
+                .MaxConcurrentRequests(() => 10)
+                .ConnectionTimeout(() => TimeSpan.FromSeconds(10))
+                .MaxQueryStringLength(() => 15)
+                .MaxRequestContentLength(() => 15)
+                .MaxUrlLength(() => 20)
+               
+    */
+
+            
+
             // To Support Cross Origin Request support
             var cors = new EnableCorsAttribute("*", "*", "*");
             config.EnableCors(cors);
@@ -30,9 +57,9 @@ namespace ThreadSafeDBService
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{StoredProcName}",
-                defaults: new { NumberToConvert = RouteParameter.Optional }
+                defaults: new { StoredProcName = RouteParameter.Optional }
             );
-
+            
             appBuilder.UseWebApi(config);
 
         }
