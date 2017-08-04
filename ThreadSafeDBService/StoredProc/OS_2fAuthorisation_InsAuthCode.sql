8747d650a649ea2d5c1095f0117d69fad504b7ca
Shanmuga <notifyshan@gmail.com>
2017-08-04 16:45:08 +1000

    ThreadSafeDB - SP updated
    ThreadSafeDB - SP updated

--- ThreadSafeDBService/StoredProc/OS_2fAuthorisation_InsAuthCode.sql
+++ ThreadSafeDBService/StoredProc/OS_2fAuthorisation_InsAuthCode.sql
@@ -1,7 +1,7 @@
 USE [MMSGCommon]
 GO
 
-/****** Object: SqlProcedure [dbo].[OS_2fAuthorisation_InsAuthCode] Script Date: 8/3/2017 3:25:54 PM ******/
+/****** Object: SqlProcedure [dbo].[OS_2fAuthorisation_InsAuthCode] Script Date: 8/4/2017 4:44:05 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -25,19 +25,14 @@ DECLARE @BaseUrl nvarchar(250);
 DECLARE @QueryString nvarchar(250);
 DECLARE @DBThreadServiceUrl nvarchar(500);
 
-SET @BaseUrl = 'http://localhost:1234/api/OS_2fAuthorisation_InsAuthCode?'
+SET @BaseUrl = 'http://10.65.120.120:1234/api/OS_2fAuthorisation_InsAuthCode?'
 SET @QueryString = 'iSystemID=' + CONVERT(VARCHAR(10), @iSystemID) + '&' +  'iCallingContext=' + CONVERT(VARCHAR(10), @iCallingContext) + '&' +'sAccountID=' + @sAccountID + '&' +'sMobileNo=' + @sMobileNo + '&' +'sAuthorisationCode=' + @sAuthorisationCode + '&' + 'iExpiryMinutes=' + CONVERT(VARCHAR(10), @iExpiryMinutes)
 SET @DBThreadServiceUrl = @BaseUrl + @QueryString;
-
 Select @DBThreadServiceUrl
 Exec sp_OACreate 'MSXML2.ServerXMLHTTP', @Object OUT;
-Exec sp_OAMethod @Object, 'open', NULL, 'get',
-	'@DBThreadServiceUrl',
- 'false'
+Exec sp_OAMethod @Object, 'open', NULL, 'get', @DBThreadServiceUrl,'false'
 Exec sp_OAMethod @Object, 'send'
 Exec sp_OAMethod @Object, 'responseText', @ResponseText OUTPUT
- 
 Select @ResponseText
- 
 Exec sp_OADestroy @Object
 END
--- ThreadSafeDBService/StoredProc/OS_2fAuthorisation_UpdValidateAuthCode.sql
+++ ThreadSafeDBService/StoredProc/OS_2fAuthorisation_UpdValidateAuthCode.sql
@@ -1,7 +1,17 @@
 USE [MMSGCommon]
 GO
 
-/****** Object: SqlProcedure [dbo].[OS_2fAuthorisation_UpdValidateAuthCode] Script Date: 8/3/2017 1:39:05 PM ******/
+/****** Object: SqlProcedure [dbo].[OS_2fAuthorisation_InsAuthCode] Script Date: 8/4/2017 12:07:19 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+USE [MMSGCommon]
+GO
+
+/****** Object: SqlProcedure [dbo].[OS_2fAuthorisation_InsAuthCode] Script Date: 8/3/2017 3:25:54 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,29 +19,31 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-CREATE PROCEDURE [dbo].[OS_2fAuthorisation_UpdValidateAuthCode]
+ALTER PROCEDURE [dbo].[OS_2fAuthorisation_UpdValidateAuthCode]
 	 @iSystemID INT
 	,@iCallingContext INT
 	,@sAccountID VARCHAR(255)
 	,@sMobileNo VARCHAR(20)
 	,@sAuthorisationCode VARCHAR(128)
-
+	,@iExpiryMinutes INT
 AS
 BEGIN
 Declare @Object as Int;
 Declare @ResponseText as Varchar(8000);
-SET @BaseUrl = "http://localhost:1234/api/OS_2fAuthorisation_UpdValidateAuthCode?"
-SET @QueryString = 'iSystemID=' + @iSystemID + '&' +  'iCallingContext=' + @iCallingContext + '&' +'sAccountID=' + @sAccountID + '&' +'sMobileNo=' + @sMobileNo + '&' +'sAuthorisationCode=' + @sAuthorisationCode 
-SET @DBThreadServiceUrl = @BaseUrl + @QueryString;
+DECLARE @BaseUrl nvarchar(250);
+DECLARE @QueryString nvarchar(250);
+DECLARE @DBThreadServiceUrl nvarchar(500);
 
-Exec sp_OACreate 'MSXML2.XMLHTTP', @Object OUT;
-Exec sp_OAMethod @Object, 'open', NULL, 'get',
-	'@DBThreadServiceUrl',
- 'false'
+SET @BaseUrl = 'http://10.65.120.120:1234/api/OS_2fAuthorisation_UpdValidateAuthCode?'
+SET @QueryString = 'iSystemID=' + CONVERT(VARCHAR(10), @iSystemID) + '&' +  'iCallingContext=' + CONVERT(VARCHAR(10), @iCallingContext) + '&' +'sAccountID=' + @sAccountID + '&' +'sMobileNo=' + @sMobileNo + '&' +'sAuthorisationCode=' + @sAuthorisationCode + '&' + 'iExpiryMinutes=' + CONVERT(VARCHAR(10), @iExpiryMinutes)
+SET @DBThreadServiceUrl = @BaseUrl + @QueryString;
+Select @DBThreadServiceUrl
+Exec sp_OACreate 'MSXML2.ServerXMLHTTP', @Object OUT;
+Exec sp_OAMethod @Object, 'open', NULL, 'get', @DBThreadServiceUrl,'false'
 Exec sp_OAMethod @Object, 'send'
 Exec sp_OAMethod @Object, 'responseText', @ResponseText OUTPUT
- 
 Select @ResponseText
- 
 Exec sp_OADestroy @Object
-	END
+END
+
+
